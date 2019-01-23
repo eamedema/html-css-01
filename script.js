0694ecbcc6500b3c6f462b12eb60e4e494d7c87a
eamedema <eamedema@gmail.com>
2019-01-23 15:10:37 -0500

    fixed modile menu display on desktop and tablet

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function myFunction() {
+  var x = document.getElementById("nav-menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
